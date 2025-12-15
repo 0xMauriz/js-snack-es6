d5c62f49595407e309864d308f0c3e4921c7a858
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-16 00:51:11 +0100

    Push della buonora, debug incluso con errori precedenti

---
+++ snack1/script/script.js
@@ -0,0 +1,23 @@
+const bicicletta = 
+{
+    "nomeBicicletta" : ['Bicicletta Intergalactic Turbo', '30Kg'],
+    "nomeBicicletta1" : ['Bicicletta BMX', '20Kg'],
+};
+//    ['Mountain Bike', '27Kg'],
+//    ['Bicicletta Intel 5', '32Kg'],
+//    ['Bicicletta Medical One', '29Kg'],
+//    ['Bicicletta Thunderbolt Galaxy', '37Kg'],
+//    ['Bicicletta Mega Saturday', '40Kg']
+// ];
+
+// console.log(bicicletta.nomeBicicletta[1]);
+// console.log(bicicletta.nomeBicicletta1[1]);
+
+
+for (let i = 0; i < bicicletta.length; i++) {
+
+    let pesoBici = bicicletta[i][1];
+
+    
+    
+}
