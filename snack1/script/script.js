f40063a504a62b24735273a0498254baaf42b5a2
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-16 01:56:37 +0100

    Ancora presenti errori

--- snack1/script/script.js
+++ snack1/script/script.js
@@ -1,23 +1,80 @@
-const bicicletta = 
-{
-    "nomeBicicletta" : ['Bicicletta Intergalactic Turbo', '30Kg'],
-    "nomeBicicletta1" : ['Bicicletta BMX', '20Kg'],
-};
-//    ['Mountain Bike', '27Kg'],
-//    ['Bicicletta Intel 5', '32Kg'],
-//    ['Bicicletta Medical One', '29Kg'],
-//    ['Bicicletta Thunderbolt Galaxy', '37Kg'],
-//    ['Bicicletta Mega Saturday', '40Kg']
-// ];
+const bicicletta = [
+    {
+        'nomeBicicletta': 'Bicicletta Intergalactic Turbo',
+        'peso': '30Kg'
 
-// console.log(bicicletta.nomeBicicletta[1]);
-// console.log(bicicletta.nomeBicicletta1[1]);
+    },
+
+    {
+        'nomeBicicletta': 'Bicicletta BMX',
+        'peso': '31Kg'
+    },
+
+    {
+        'nomeBicicletta': 'Mountain Bike',
+        'peso': '27Kg'
+    },
+
+    {
+        'nomeBicicletta': 'Bicicletta Intel 5',
+        'peso': '32Kg'
+    },
+
+    {
+        'nomeBicicletta': 'Bicicletta Medical One',
+        'peso': '29Kg'
+    },
+
+    {
+        'nomeBicicletta': 'Bicicletta Thunderbolt Galaxy',
+        'peso': '37Kg'
+    },
+
+    {
+        'nomeBicicletta': 'Bicicletta Mega Saturday',
+        'peso': '40Kg'
+    },
+
+];
+
+let minorWeigth;
+let pesoBici;
+
+// console.log(bicicletta[0].peso);
+//console.log(bicicletta[1].peso);
+
+// console.log(bicicletta[1].peso);
+
+console.log(bicicletta.length);
+
 
 
 for (let i = 0; i < bicicletta.length; i++) {
 
-    let pesoBici = bicicletta[i][1];
 
+    pesoBici = parseInt(bicicletta[i].peso);
+
+    console.log(pesoBici);
     
-    
+
+    for (let y = 1; y < bicicletta.length; y++) {
+
+        let pesoBiciCompare = parseInt(bicicletta[y].peso);
+        
+    console.log(pesoBiciCompare);
+
+        if (pesoBici < pesoBiciCompare) {
+
+            minorWeigth = pesoBici;
+
+        }
+
+        else if (pesoBici > pesoBiciCompare) {
+            minorWeigth = pesoBiciCompare;
+        }
+
+
+    }
 }
+
+console.log(minorWeigth);
