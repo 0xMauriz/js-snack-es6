498b384dde3a650ec115db8f4a9b0d991e3b7652
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-16 02:44:16 +0100

    Esercizio completato. Ho capito come funzionano gli oggetti, sono in molti aspetti molto simili agli array e seguono una logica quasi uguale ma sono più completi di informazioni.

--- snack2/script/script.js
+++ snack2/script/script.js
@@ -45,5 +45,22 @@ for (let i = 0; i < squadreDiCalcio.length; i++) {
 
     
     squadreDiCalcio[i].falliSubiti = inrangeRandomNumberGenerator(0,20);
+    console.log(squadreDiCalcio[i].falliSubiti);
+    
 
 }
+
+// Vengono immessi in un nuovo Array solo i nomi dei giocatori con i falli subiti
+
+const nomiDelleSqadreEFalliSubiti = [
+];
+
+
+
+for (let i = 0; i < squadreDiCalcio.length; i++) {
+    nomiDelleSqadreEFalliSubiti.push(squadreDiCalcio[i].nomeSquadra);
+    nomiDelleSqadreEFalliSubiti.push(squadreDiCalcio[i].falliSubiti);
+}
+
+
+console.log(nomiDelleSqadreEFalliSubiti);
